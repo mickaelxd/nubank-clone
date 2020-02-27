eb244d17c4bf5ed4fd4d4efdee9c39414ca2904c
Mickael Rocha <mickael.n.xd@gmail.com>
2020-02-27 18:32:24 -0300

    Card animation

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
-
 import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import {Animated} from 'react-native';
+import {PanGestureHandler, State} from 'react-native-gesture-handler';
+
+import Header from '~/Components/Header';
+import Tabs from '~/Components/Tabs';
+import Menu from '~/Components/Menu';
 
 import {
   Container,
@@ -14,36 +20,89 @@ import {
   Annotation,
 } from './styles';
 
-import Header from '~/Components/Header';
-import Tabs from '~/Components/Tabs';
-import Menu from '~/Components/Menu';
-
 export default function Main() {
+  let offset = 0;
+
+  const translateY = new Animated.Value(0);
+
+  const animatedEvent = Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationY: translateY,
+        },
+      },
+    ],
+    {useNativeDriver: true},
+  );
+
+  function onHandlerStateChanged(event) {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      let opened = false;
+      const {translationY} = event.nativeEvent;
+
+      offset += translationY;
+
+      if (translationY >= 100) {
+        opened = true;
+      } else {
+        translateY.setValue(offset);
+        translateY.setOffset(0);
+        offset: 0;
+      }
+
+      Animated.timing(translateY, {
+        toValue: opened ? 380 : 0,
+        duration: 200,
+        useNativeDriver: true,
+      }).start(() => {
+        offset = opened ? 380 : 0;
+        translateY.setOffset(offset);
+        translateY.setValue(0);
+      });
+    }
+  }
+
   return (
     <Container>
       <Header />
 
       <Content>
-        <Menu />
-        <Card>
-          <CardHeader>
-            <Icon name="attach-money" size={28} color="#666" />
-            <Icon name="visibility-off" size={28} color="#666" />
-          </CardHeader>
-          <CardContent>
-            <Title>Saldo disponível</Title>
-            <Description>R$ 330.123,90</Description>
-          </CardContent>
-          <CardFooter>
-            <Annotation>
-              Transferência de R$ 149.148,00 recebida de Cesar Vialpando hoje às
-              18:37
-            </Annotation>
-          </CardFooter>
-        </Card>
+        <Menu translateY={translateY} />
+        <PanGestureHandler
+          onGestureEvent={animatedEvent}
+          onHandlerStateChange={onHandlerStateChanged}>
+          <Card
+            style={{
+              transform: [
+                {
+                  translateY: translateY.interpolate({
+                    inputRange: [-350, 0, 380],
+                    outputRange: [-50, 0, 380],
+                    extrapolate: 'clamp',
+                  }),
+                },
+              ],
+            }}>
+            <CardHeader>
+              <Icon name="attach-money" size={28} color="#666" />
+              <Icon name="visibility-off" size={28} color="#666" />
+            </CardHeader>
+            <CardContent>
+              <Title>Saldo disponível</Title>
+              <Description>R$ 330.123,90</Description>
+            </CardContent>
+            <CardFooter>
+              <Annotation>
+                Transferência de R$ 149.148,00 recebida de Cesar Vialpando hoje
+                às 18:37
+              </Annotation>
+            </CardFooter>
+          </Card>
+        </PanGestureHandler>
       </Content>
 
-      <Tabs />
+      <Tabs translateY={translateY} />
     </Container>
   );
 }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,3 +1,4 @@
+import {Animated} from 'react-native';
 import styled from 'styled-components/native';
 import {getStatusBarHeight} from 'react-native-iphone-x-helper';
 
@@ -13,7 +14,7 @@ export const Content = styled.View`
   max-height: 400px;
   z-index: 5;
 `;
-export const Card = styled.View`
+export const Card = styled(Animated.View)`
   flex: 1;
   background: #fff;
   border-radius: 4px;
@@ -22,7 +23,7 @@ export const Card = styled.View`
   position: absolute;
   left: 0;
   right: 0;
-  top: 380px;
+  top: 0;
 `;
 export const CardHeader = styled.View`
   flex-direction: row;
