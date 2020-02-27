ba5b8826af5f0ddf261ac9c373a4b804d4f88277
Mickael Rocha <mickael.n.xd@gmail.com>
2020-02-27 17:49:41 -0300

    Menu and QRCode

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,13 +1,48 @@
 import React from 'react';
-import {Container} from './styles';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  Title,
+  Description,
+  Annotation,
+} from './styles';
 
 import Header from '~/Components/Header';
 import Tabs from '~/Components/Tabs';
+import Menu from '~/Components/Menu';
 
 export default function Main() {
   return (
     <Container>
       <Header />
+
+      <Content>
+        <Menu />
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 330.123,90</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferência de R$ 149.148,00 recebida de Cesar Vialpando hoje às
+              18:37
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
       <Tabs />
     </Container>
   );
